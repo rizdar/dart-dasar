f1ffeff98d3af49a30fa80eaa67ff9b1fb3a8724
Rizki Darmawan <rizdar.contact@gmail.com>
2024-04-28 10:09:26 +0700

    belajar list

---
+++ bin/list.dart
@@ -0,0 +1,15 @@
+void main() {
+  var mahasiswa = ["rizki", "darmawan", "rio"];
+  print(mahasiswa);
+
+  List<String> alamat = ["Ciputat", "Jakarta", "Bogor"];
+  List<int> nomorList = [4, 4, 45, 58];
+
+  alamat.add("Bekasi");
+
+  print(alamat);
+  print(alamat[1]);
+
+  print(alamat);
+  print(nomorList);
+}
